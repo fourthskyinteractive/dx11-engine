51cf68fdd3a23b2b0a9670f722293b54c12ad11e
MikeSawayda <mikesawayda@gmail.com>
2013-02-28 08:19:50 +0000

    Adding some shader stuff

--- DirectX11 Engine/DirectX11 Engine/Src/Renderer/ShaderManager.cpp
+++ DirectX11 Engine/DirectX11 Engine/Src/Renderer/ShaderManager.cpp
@@ -1,4 +1,5 @@
 #include "ShaderManager.h"
+#include "D3D11Renderer.h"
 #include <windows.h>
 #include <d3d11.h>
 
@@ -7,10 +8,17 @@ vector<ID3D11VertexShader*> ShaderManager::vertexShaders;
 void ShaderManager::AddEffect(char* _filePath)
 {
 	////CONVERTING THE char* to a wchar*
-	//size_t origsize = strlen(_filePath) + 1;
-	//const size_t newSize = 100;
-	//size_t convertedChars = 0;
-	//wchar_t wcString[newSize];
-	//mbstowcs_s(&convertedChars, wcString, origsize, _filePath, _TRUNCATE);
-	//wcscat_s(wcString, L"");
+	size_t origsize = strlen(_filePath) + 1;
+	const size_t newSize = 100;
+	size_t convertedChars = 0;
+	wchar_t wcString[newSize];
+	mbstowcs_s(&convertedChars, wcString, origsize, _filePath, _TRUNCATE);
+	wcscat_s(wcString, L"");
+
+	ID3D11VertexShader* VS;
+	ID3DBlob* VSBuffer;
+
+	D3DReadFileToBlob(wcString, &VSBuffer);
+	D3D11Renderer::d3dDevice->CreateVertexShader(VSBuffer->GetBufferPointer(), VSBuffer->GetBufferSize(), NULL, &VS);
+	D3D11Renderer::d3dImmediateContext->VSSetShader(VS, NULL, NULL);
 }
