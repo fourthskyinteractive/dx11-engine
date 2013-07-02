94499502bb9a61b79ef85a9d2c382af6a63d9993
mikesawayda@gmail.com <mikesawayda@gmail.com@322499af-557f-806e-b74c-71c733c0e401>
2013-07-02 23:53:18 +0000

    Working on the Diamond-Square algorithm for my terrain generator.

--- trunk/DirectX11 Engine/DirectX11 Engine/Src/Utility/Misc/TerrainGenerator.cpp
+++ trunk/DirectX11 Engine/DirectX11 Engine/Src/Utility/Misc/TerrainGenerator.cpp
@@ -1,5 +1,7 @@
 #include "TerrainGenerator.h"
 
+int		TerrainGenerator::slicesMade;
+
 TerrainGenerator::TerrainGenerator()
 {
 
@@ -10,7 +12,7 @@ TerrainGenerator::~TerrainGenerator()
 
 }
 
-void TerrainGenerator::CreateTerrain(float _width, float _height, int _numberOfSegments, int _smoothingFactor, XMFLOAT3 _centerPoint, vector<XMFLOAT3>& _vertices)
+void TerrainGenerator::CreateTerrain(float _width, float _height, int _numberOfSegments, float _smoothingFactor, XMFLOAT3 _centerPoint, vector<XMFLOAT3>& _vertices)
 {
 	//NUMBER OF SEGMENTS IS PER SIDE
 	
@@ -45,11 +47,32 @@ void TerrainGenerator::CreateTerrain(float _width, float _height, int _numberOfS
 	_vertices[BL].y = 20;
 	_vertices[BR].y = 20;
 
-	SquareStep(TL, TR, BL, BR, _vertices);
+	SquareStep(TL, TR, BL, BR, _smoothingFactor, _vertices);
 }
 
-void TerrainGenerator::SquareStep(int _TL, int _TR, int _BL, int _BR, vector<XMFLOAT3>& _vertices)
+void TerrainGenerator::SquareStep(int _TL, int _TR, int _BL, int _BR, float _smoothingFactor, vector<XMFLOAT3>& _vertices)
 {
+	slicesMade += 1;
+
 	int centerPoint = ((_BR - _BL) / 2) + _BL;
 	centerPoint += (_BR - _BL + 1) * ((_BR - _BL) / 2);
+
+	float averageOfCorners = (_vertices[_BR].y + _vertices[_BL].y + _vertices[_TR].y + _vertices[_TL].y) / 4.0f;
+	_vertices[centerPoint].y = averageOfCorners + _smoothingFactor;
+
+	DiamondStep(_TL, _TR, _BL, _BR, centerPoint, _smoothingFactor, _vertices);
+}
+
+void TerrainGenerator::DiamondStep(int _TL, int _TR, int _BL, int _BR, int _centerPoint, float _smoothingFactor, vector<XMFLOAT3>& _vertices)
+{
+	int halfWidth = ((_TR - _TL) / 2);
+	int TM = _TL + halfWidth;
+	int RM = _centerPoint + halfWidth;
+	int LM = _centerPoint - halfWidth;
+	int BM = _BL + halfWidth;
+
+	_vertices[TM].y = (_vertices[_TL].y + _vertices[_TR].y / 2.0f) + _smoothingFactor;
+	_vertices[RM].y = (_vertices[_TR].y + _vertices[_BR].y / 2.0f) + _smoothingFactor;
+	_vertices[LM].y = (_vertices[_TL].y + _vertices[_BL].y / 2.0f) + _smoothingFactor;
+	_vertices[BM].y = (_vertices[_BL].y + _vertices[_BR].y / 2.0f) + _smoothingFactor;
 }
