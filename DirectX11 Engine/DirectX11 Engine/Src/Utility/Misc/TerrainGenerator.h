d7f3980be947b8dababaa7576d161456d7c2dc3a
MikeSawayda <mikesawayda@gmail.com>
2013-07-03 04:59:24 +0000

    Finished the Generate Terrain algorithm.

--- DirectX11 Engine/DirectX11 Engine/Src/Utility/Misc/TerrainGenerator.h
+++ DirectX11 Engine/DirectX11 Engine/Src/Utility/Misc/TerrainGenerator.h
@@ -7,22 +7,45 @@
 #include <dxgi.h>
 using namespace DirectX;
 #include <vector>
+#include <queue>
 using namespace std;
 
+struct Square
+{
+	int BL;
+	int width;
+};
+
+struct Diamond
+{
+	int centerPoint;
+	int width;
+};
 
 class TerrainGenerator
 {
 public:
+
 	TerrainGenerator();
 	TerrainGenerator(const TerrainGenerator&){};
 	~TerrainGenerator();
 
 	static void CreateTerrain(float _width, float _height, int _numberOfSegments, float _smoothingFactor, XMFLOAT3 _centerPoint, vector<XMFLOAT3>& _vertices);
 	static void SquareStep(int _TL, int _TR, int _BL, int _BR, float _smoothingFactor, vector<XMFLOAT3>& _vertices);
+	static void SquareStep(int _centerPoint, int _width, float _smoothingFactor, vector<XMFLOAT3>& _vertices);
 	static void DiamondStep(int _TL, int _TR, int _BL, int _BR, int _centerPoint, float _smoothingFactor, vector<XMFLOAT3>& _vertices);
+	static void DiamondStep(int _BL, int _width, float _smoothingFactor, vector<XMFLOAT3>& _vertices);
 
 private:
+	static queue<Square> squares;
+	static queue<Diamond> diamonds;
+
 	static int slicesMade;
+	static float width;
+	static float height;
+	static int numberOfSegments;
+	static float smoothingFactor;
+	static XMFLOAT3 centerPoint;
 };
 
 #endif
