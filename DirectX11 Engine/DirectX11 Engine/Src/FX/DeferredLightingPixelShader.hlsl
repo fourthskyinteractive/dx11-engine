f36e68efb1aeca204f91026fedd4faaa81b607d2
MikeSawayda <mikesawayda@gmail.com>
2013-10-10 03:46:34 +0000

    Messing with the shaders

--- DirectX11 Engine/DirectX11 Engine/Src/FX/DeferredLightingPixelShader.hlsl
+++ DirectX11 Engine/DirectX11 Engine/Src/FX/DeferredLightingPixelShader.hlsl
@@ -1,29 +1,40 @@
-Texture2D normalTexture				: register(t0);
-Texture2D diffuseAlbedoTexture		: register(t1);
-Texture2D specularAlbedoTexture		: register(t2);
-Texture2D positionTexture			: register(t3);
+//Globals
+Texture2D normalTexture : register(t0);
+Texture2D diffuseAlbedoTexture : register(t1);
+Texture2D specularAlbedoTexture : register(t2);
+Texture2D positionTexture : register(t3); 
 
 //Constants
-cbuffer LightParams
+cbuffer LightandCameraParams
 {
-	float3 lightPosition;
-	float3 lightColor;
-	float3 lightDirection;
-	float2 spotLightAngles;
+	//(POINT LIGHT, SPOT LIGHT, DIRECTIONAL LIGHT, AMBIENT LIGHT)
+	float4 lightType;
+	float4 lightPos;
+	float4 lightColor;
+	float4 lightDirection;
+	float4 spotlightAngles;
 	float4 lightRange;
+	float4 cameraPos;
 };
 
-cbuffer cameraParams
+//TYPEDEFS
+struct PixelIn
 {
-	float3 cameraPos;
+	float4 pos		: SV_POSITION;
+	//float2 tex	: TEXCOORD0;
 };
 
-//Helper function for extracting the G-Buffer attributes
+struct PixelOut
+{
+	float4 color : SV_Target0;
+};
+
 void GetGBufferAttributes(in float2 screenPos, out float3 normal,
 						  out float3 position,
 						  out float3 diffuseAlbedo, out float3 specularAlbedo,
 						  out float specularPower)
 {
+	//Determine our indices for sampling the texture based on current screen position
 	int3 sampleIndices = int3(screenPos.xy, 0);
 
 	normal = normalTexture.Load(sampleIndices).xyz;
@@ -31,59 +42,108 @@ void GetGBufferAttributes(in float2 screenPos, out float3 normal,
 	diffuseAlbedo = diffuseAlbedoTexture.Load(sampleIndices).xyz;
 	float4 spec = specularAlbedoTexture.Load(sampleIndices);
 
-	specularAlbedo = spec.xyz;
+	//specularAlbedo = (lightColor * 0.5f);
+	specularAlbedo = ((lightColor.xyz * diffuseAlbedo) *  0.5f);;
 	specularPower = spec.w;
 }
 
-//Calculates the lighting term for a single G-Buffer texel
-float3 CalcLighting(in float3 normal,
-					in float3 position,
-					in float3 diffuseAlbedo,
-					in float3 specularAlbedo,
-					in float specularPower)
+float3 CalculateLighting(in float3 normal,
+						 in float3 position,
+						 in float3 diffuseAlbedo,
+						 in float3 specularAlbedo,
+						 in float specularPower)
 {
+// 	float cutoff = 0.005;
+// 
+// 	float3 LV = float4(float4(lightPos, 1.0f) - float4(position, 1.0f)).xyz;
+// 
+// 	float dist = length(LV);
+// 
+// 	float edgeDist = max(dist - lightRange.x, 0);
+// 
+// 	LV /= dist;
+// 	float denom = edgeDist / lightRange.x + 1;
+// 	float att = 1 / (denom * denom);
+// 	att = (att - cutoff) / (1 - cutoff);
+// 	att = max(att, 0);
+// 
+// 	float diffuse = max(dot(LV, normal), 0);
+// 
+// 	float3 Half = normalize(LV + normalize(cameraPos - position));
+// 	float specular = pow(saturate(dot(Half, normal)), 10);
+// 
+// 	return (lightColor * diffuse * att) + ((lightColor * 1.0f) * specular * 0.2);
+
 	float3 L = 0;
 	float attenuation = 1.0f;
-#if POINTLIIGHT || SPOTLIGHT
-	
-		L = lightPos - position;
-	
-#elif DIRECTIONALLIGHT
-	
-		L = -lightDirection;
-#endif
 
-#if SPOTLIGHT
-	
-		float3 L2 = lightDirection;
+	//Ambient Light
+	if(lightType.w == 1.0f)
+	{
+		return lightColor.xyz;
+	}
+
+	//If point light or spot light
+ 	if(lightType.x == 1.0f || lightType.y == 1.0f)
+	{
+ 		L = lightPos.xyz - position;
+		float dist = length(L);
+		attenuation = max(0, 1.0f - (dist / lightRange.x));
+		L /= dist;
+	}
+	//If directional light
+	else if(lightType.z == 1.0f)
+	{
+		L = -lightDirection.xyz;
+	}
+	else
+	{
+
+	}
+	//If spot light
+	if(lightType.y == 1.0f)
+	{
+		float3 L2 = lightDirection.xyz;
 		float rho = dot(-L, L2);
-		attenuation *= saturate((rho - spotlightAngles.y)
-									/ (spotlightAngles.x - spotlightAngles.y));
-#endif
+		attenuation *= saturate((rho - spotlightAngles.y) /
+								(spotlightAngles.x - spotlightAngles.y));
+	}
 
-	float nDotL = saturate(dot(L, normal));
-	float3 diffuse = nDotL * lightColor * diffuseAlbedo;
+	float nDotL = saturate(dot(normal, L));
+	float3 diffuse = nDotL * lightColor.xyz * diffuseAlbedo;
 
-	float3 V = cameraPos - position;
+	//Calculate the speculat term
+	float3 V = cameraPos.xyz - position;
 	float3 H = normalize(L + V);
-	float3 specular = pow(saturate(dot(normal, H)), 64) 
-							* lightColor * specularAlbedo.xyz * nDotL;
-	return (diffuse + specular) * attenuation;
+	float specNormalizationFactor = ((specularPower + 8.0f) / ( 8.0f * 3.14159265f));
+
+	float3 specular = pow(saturate(dot(normal, H)), specularPower)
+							* specNormalizationFactor * lightColor.xyz * specularAlbedo.xyz * nDotL;
+
+	//Final value is the sum of the albedo and diffuse with attenuation applied.
+	return(diffuse + specular) * attenuation;
 }
 
-float4 PS(in float4 screenPos : SV_POSITION) : SV_Target0
+
+PixelOut PS(PixelIn input)
 {
+	PixelOut pOut;
+	
 	float3 normal;
 	float3 position;
 	float3 diffuseAlbedo;
 	float3 specularAlbedo;
 	float specularPower;
 
-	GetGBufferAttributes(screenPos.xy, normal, position, 
-							diffuseAlbedo, specularAlbedo, specularPower);
+	//Sample the G-Buffer properties from the textures
+	GetGBufferAttributes(input.pos.xy, normal, position, diffuseAlbedo,
+						specularAlbedo, specularPower);	
 
-	float3 lighting = CalcLighting(normal, position, diffuseAlbedo,
-									specularAlbedo, specularPower);
+	float3 lighting = CalculateLighting(normal, position, diffuseAlbedo, specularAlbedo, specularPower);
 
-	return float4(lighting, 1.0f);
+	lighting *= diffuseAlbedo;
+
+	pOut.color = float4(lighting, 1.0f);
+
+	return pOut;
 }
--- DirectX11 Engine/DirectX11 Engine/Src/FX/DeferredLightingVertexShader.hlsl
+++ DirectX11 Engine/DirectX11 Engine/Src/FX/DeferredLightingVertexShader.hlsl
@@ -1,28 +1,4 @@
-#pragma pack_matrix(row_major)
-
-cbuffer Transforms
+float4 VS( float4 pos : POSITION ) : SV_POSITION
 {
-	matrix worldMatrix;
-	matrix viewProjectionMatrix;
-};
-
-//Input/Output structures
-struct VSIn
-{
-	float4 position			: POSITION;
-};
-
-struct VSOut
-{
-	float4 position			: SV_POSITION;
-};
-
-VSOut VS(in VSIn input)
-{
-	VSOut output;
-
-	output.position = mul(input.position, worldMatrix);
-	output.position = mul(output.position, viewProjectionMatrix);
-
-	return output;
+	return float4(0.0f, 0.0f, 0.0f, 0.0f);
 }
