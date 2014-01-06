021b49055e5ba151ed4e0951df75a0c1ba0508cb
mikesawayda@gmail.com <mikesawayda@gmail.com@322499af-557f-806e-b74c-71c733c0e401>
2014-01-06 20:55:44 +0000

    Microsoft and their retardedness!!  Fixed the assumption that you are in Windows 8 when you use Visual Studio 2012.

--- trunk/DirectX11 Engine/DirectX11 Engine/Src/Utility/DDS Texture Loader/DDSTextureLoader.cpp
+++ trunk/DirectX11 Engine/DirectX11 Engine/Src/Utility/DDS Texture Loader/DDSTextureLoader.cpp
@@ -25,9 +25,11 @@
 
 #include "DDSTextureLoader.h"
 
-#ifndef _WIN32_WINNT_WIN8
-#define _WIN32_WINNT_WIN8 0x0602
-#endif
+// #ifndef _WIN32_WINNT_WIN8
+// #define _WIN32_WINNT_WIN8 0x0602
+// #endif
+
+#define _WIN32_WINNT 0x0601
 
 #if (_WIN32_WINNT >= _WIN32_WINNT_WIN8) && !defined(DXGI_1_2_FORMATS)
 #define DXGI_1_2_FORMATS
@@ -163,13 +165,13 @@ static HRESULT LoadTextureDataFromFile( _In_z_ const wchar_t* fileName,
     }
 
     // open the file
-// #if (_WIN32_WINNT >= _WIN32_WINNT_WIN8)
-//     ScopedHandle hFile( safe_handle( CreateFile2( fileName,
-//                                                   GENERIC_READ,
-//                                                   FILE_SHARE_READ,
-//                                                   OPEN_EXISTING,
-//                                                   nullptr ) ) );
-// #else
+#if (_WIN32_WINNT >= _WIN32_WINNT_WIN8)
+    ScopedHandle hFile( safe_handle( CreateFile2( fileName,
+                                                  GENERIC_READ,
+                                                  FILE_SHARE_READ,
+                                                  OPEN_EXISTING,
+                                                  nullptr ) ) );
+#else
     ScopedHandle hFile( safe_handle( CreateFileW( fileName,
                                                   GENERIC_READ,
                                                   FILE_SHARE_READ,
@@ -177,7 +179,7 @@ static HRESULT LoadTextureDataFromFile( _In_z_ const wchar_t* fileName,
                                                   OPEN_EXISTING,
                                                   FILE_ATTRIBUTE_NORMAL,
                                                   nullptr ) ) );
-//#endif
+#endif
 
     if ( !hFile )
     {
