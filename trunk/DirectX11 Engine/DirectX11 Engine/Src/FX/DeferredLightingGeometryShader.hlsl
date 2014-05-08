d45f550754ea540c4ec85d7131ca57f6681442f3
mikesawayda@gmail.com <mikesawayda@gmail.com@322499af-557f-806e-b74c-71c733c0e401>
2014-05-08 22:34:52 +0000

    Fixed the texture coordinates in the geometry shader

--- trunk/DirectX11 Engine/DirectX11 Engine/Src/FX/DeferredLightingGeometryShader.hlsl
+++ trunk/DirectX11 Engine/DirectX11 Engine/Src/FX/DeferredLightingGeometryShader.hlsl
@@ -1,7 +1,7 @@
 struct GSOutput
 {
 	float4 pos : SV_POSITION;
-	float3 tex : TEXCOORD0;
+	float2 tex : TEXCOORD0;
 	float3 skyboxTex : TEXCOORD1;
 };
 
@@ -22,22 +22,22 @@ void GS(
 						1, -1, 0.5, 1};
 
 		gsOut.pos = verts[0];
-		gsOut.tex = verts[0];
+		gsOut.tex = float2(0.0f, 0.0f);
 		gsOut.skyboxTex = mul(verts[0], invViewProjectionMatrix);
 		output.Append(gsOut);
 
 		gsOut.pos = verts[1];
-		gsOut.tex = verts[1];
+		gsOut.tex = float2(1.0f, 0.0f);
 		gsOut.skyboxTex = mul(verts[1], invViewProjectionMatrix);
 		output.Append(gsOut);
 
 		gsOut.pos = verts[2];
-		gsOut.tex = verts[2];
+		gsOut.tex = float2(0.0f, 1.0f);
 		gsOut.skyboxTex = mul(verts[2], invViewProjectionMatrix);
 		output.Append(gsOut);
 
 		gsOut.pos = verts[3];
-		gsOut.tex = verts[3];
+		gsOut.tex = float2(1.0f, 1.0f);
 		gsOut.skyboxTex = mul(verts[3], invViewProjectionMatrix);
 		output.Append(gsOut);
 }
